d6feabeb5de920059e41abcdeea4ad7be7e46ae8
99865@mail.ru <a903560>
2016-05-27 16:25:26 +0300

    training files 1

--- if-elst.php
+++ if-elst.php
@@ -5,7 +5,23 @@ $b = 15;
 
 if($a < $b) echo '$a < $b'.'<br>';
 
+if($a == $b) echo '$a == $b'.'<br>';
+else echo '$a != $b'.'<br>';
 
+echo '<br>';
+
+$c = 1;
+$d = 2;
+$e = 3;
+
+if ($c == 1) echo '$c == 1'.'<br>';
+    elseif ($d == 2) echo '$d == 2'.'<br>';
+    elseif ($e == 3) echo '$e == 3'.'<br>';
+else echo 'Условия не выполнились<br>';
+
+
+
+echo '<br>Эта строка находится за блоком условий<br>';
 
 
 ?>
