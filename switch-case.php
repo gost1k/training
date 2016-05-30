c3e5fc1995b96d08df37c71ee7a372d838743868
99865@mail.ru <a903560>
2016-05-30 20:21:14 +0300

    training files 1

---
+++ switch-case.php
@@ -0,0 +1,23 @@
+<?php
+$year = 2008;
+
+switch($year)
+{
+    case 2008;
+        echo 'Год 2008<br>';
+        echo 'Список статей<br>';
+        break;
+    case 2009;
+        echo 'Год 2009<br>';
+        echo 'Список статей<br>';
+        break;
+    case 2010;
+        echo 'Год 2010<br>';
+        echo 'Список статей<br>';
+        break;
+    default:
+        echo 'Неправильно указан год<br>';
+}
+
+echo 'Эта строка находится за конструкцией switch<br>';
+?>
