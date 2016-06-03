89391e7572b532671db48c9b3fd862ca509156ed
99865@mail.ru <a903560>
2016-06-03 14:59:16 +0300

    training files 1

---
+++ functions4.php
@@ -0,0 +1,16 @@
+<?php
+
+printf('текстовая строка');
+
+echo '<br><br>';
+
+$int = 1500;
+
+$str = 'Текст';
+
+$float = 157.3692;
+
+$perc = 16;
+
+printf('Значения переменных: <br> int = %d <br> str = %s <br> float = %.4f <br> perc = %d%% <br> int2 = %d',$int, $str, $float, $perc, $int);
+?>
