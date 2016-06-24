9a85a95be807c05df3ba40036ac2b184ac94b9b9
99865@mail.ru <a903560>
2016-06-24 20:38:20 +0300

    Простое ооп

---
+++ base/oop/construct.php
@@ -0,0 +1,89 @@
+<?php
+//Simple class
+/*
+    class Users{
+        public $variable;
+        function __construct()
+        {
+            $this->variable = "Hello World";
+        }
+        function show(){
+            return $this->variable;
+        }
+    }
+
+$user = new Users();
+print $user->show();
+
+
+*/?>
+
+
+<?php
+//Класс человек
+class Man{
+    public $name;
+    public $surname;
+    public $age;
+
+    function __construct()
+    {
+        $this->name = "Максим";
+        $this->surname = "Таранов";
+        $this->age = 26;
+    }
+
+    function show(){
+        return $this->name;
+        return $this->surname;
+        return $this->age;
+    }
+}
+
+$iam = new Man();
+?>
+<p>
+    Приветствую вас <?php echo $iam->name; echo " "; echo $iam->surname; ?> <br>
+    Ваш возраст <?php print $iam->age; ?>
+</p>
+<!-- Конец класса -->
+
+
+<?php
+    class Users{
+        public $variable;
+        function __construct($text)
+        {
+            $this->variable = $text;
+        }
+        function show(){
+            return $this->variable;
+        }
+    }
+
+$user = new Users("Просто тест");
+echo $user->show();
+
+
+
+//Calculator
+$n1 = 3;
+$n2 = 6;
+
+    class Calculator{
+        public $a;
+        public $b;
+        public $c;
+    function __construct($a,$b)
+    {
+        $this->c = $a+$b;
+    }
+        function show(){
+            return $this->c;
+        }
+    }
+
+$calc = new Calculator(3,6);
+echo "<br>$n1 + $n2 = ".$calc->show();
+
+?>
