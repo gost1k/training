fc9dec97d28c5124a733023ab6b294025a530fd9
TarAn <9986544@gmail.com>
2025-10-31 20:54:02 +0300

    moved to own repo

--- Vue/Vue2/vuejs/WebDeveloperBlog/Part9 - Adaptive Menu/index.js
+++
@@ -1,8 +0,0 @@
-new Vue({
-    el: '#app',
-    data:
-    {
-        items: ['Home', 'About', 'Blog', 'Map', 'Contacts' ],
-        show:false,
-    },
-});
--- Vue/Vue2/vuejs/WebDeveloperBlog/index.js
+++
@@ -1,8 +0,0 @@
-new Vue({
-    el: '#app',
-    data: {
-        showPass: false,
-        password: '1a2b3c4d'
-    }
-
-})
